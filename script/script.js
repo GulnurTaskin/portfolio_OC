4f6aa224f057bc8e8563014cca5be396df78e87e
Gulnur <taskin.gulnur@gmail.com>
2026-05-22 17:26:46 +0200

    l'accessibilité, des contrastes et du HTML sémantique

--- script/script.js
+++ script/script.js
@@ -42,8 +42,13 @@ function createSkillsFromJSON() {
                 card.innerHTML = `
                     <div class="card skillsText">
                         <div class="card-body">
-                            <img src="./images/${item.image}" />
-                            <h4 class="card-title mt-3">${item.title}</h4>
+                           <img 
+class="card-img-top"
+src="images/${item.image}"
+alt="${item.text}"
+loading="lazy"
+style="width:100%">
+                            <h3 class="card-title mt-3">${item.title}</h3>
                             <p class="card-text mt-3">${item.text}</p>
                         </div>
                     </div>
@@ -79,7 +84,7 @@ function createPortfolioFromJSON() {
                     <div class="card portfolioContent">
                         <img class="card-img-top" src="images/${item.image}" alt="${item.title}">
                         <div class="card-body">
-                            <h4 class="card-title">${item.title}</h4>
+                            <h3 class="card-title">${item.title}</h3>
                             <p class="card-text">${item.text}</p>
                             <div class="text-center">
                                 <a href="${item.link}" target="_blank" class="btn btn-success">Voir le projet</a>
